e04b427b0f7f5011e5943afb2f1880e361ae4579
james victor <victorjames40v@gmail.com>
2024-07-02 06:32:46 +0100

    wrote my tech goals for th next two years

--- javascript.js
+++ javascript.js
@@ -7,10 +7,19 @@ function getTime(){
         seconds:currentTime.getUTCSeconds()
     }
     
-    
+    // geting time AM Or PM
+
+    function PmAm(){
+        const options={hour: "numeric", minute: "numeric",second: "numeric", hour12:true };
+        const timeString=currentTime.toLocaleTimeString('en-US', options)
+
+        const amPm=timeString.slice(-2)
+        return amPm
+    }
+    PmAm()
 
     const daysOfTheWeek=["","Monday","Tusday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-    const Time=`${timeinfo.hour}:${timeinfo.minutes}:${timeinfo.seconds}`
+    const Time=`${timeinfo.hour}:${timeinfo.minutes}:${timeinfo.seconds} ${PmAm()}`
 
     document.querySelector(`[data-testid="currentTimeUTC"]`).innerHTML=Time;
     document.querySelector(`[data-testid="currentDay"]`).innerHTML=daysOfTheWeek[timeinfo.day]
